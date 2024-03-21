152646d8ff935dffe4f53811bc77e6c940b04ca8
Davron-Adbusalomov <2110117@newuzbekistanuniversity.uz>
2024-03-21 10:51:55 +0500

    first

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,6 +1,6 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/Davron-Adbusalomov/madule1-gitTask.git";
+    public static String REPO_LINK = "https://github.com/Uchiha8/stage2-module1-git";
 
 }
